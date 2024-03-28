b65099fa391562227ca1920c10f744eccfba5c39
Priyanshu Singh <reveur.guy99@gmail.com>
2024-03-28 15:07:23 +0530

    updated config files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,48 +1,48 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       container: {
         center: true,
-        padding: "2rem",
+        padding: '2rem',
         screens: {
-          "2xl": "1400px",
+          '2xl': '1400px',
         },
       },
       extend: {
         keyframes: {
-          "accordion-down": {
+          'accordion-down': {
             from: { height: 0 },
-            to: { height: "var(--radix-accordion-content-height)" },
+            to: { height: 'var(--radix-accordion-content-height)' },
           },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
+          'accordion-up': {
+            from: { height: 'var(--radix-accordion-content-height)' },
             to: { height: 0 },
           },
         },
         animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
+          'accordion-down': 'accordion-down 0.2s ease-out',
+          'accordion-up': 'accordion-up 0.2s ease-out',
         },
       },
     },
   },
   plugins: [
-    require("tailwindcss-animate"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
+    require('tailwindcss-animate'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
-}
-export default config
+};
+export default config;
