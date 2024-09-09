4823f5c0b9d3fde9f4f905d2339afd659450543b
Priyanshu Singh <reveur.guy99@gmail.com>
2024-09-09 19:50:20 +0530

    minor ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <Image
-          className="dark:invert"
+          className="invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
@@ -15,7 +15,7 @@ export default function Home() {
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
             Get started by editing{' '}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
+            <code className="rounded bg-white/[.1] px-1 py-0.5 font-semibold">
               app/page.tsx
             </code>
             .
@@ -25,12 +25,12 @@ export default function Home() {
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
-            className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
+            className="group flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-primary px-4 text-sm text-background transition-colors hover:bg-[#ccc] hover:text-black sm:h-12 sm:px-5 sm:text-base"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
             <Image
-              className="dark:invert"
+              className="group-hover:invert"
               src="https://nextjs.org/icons/vercel.svg"
               alt="Vercel logomark"
               width={20}
@@ -39,7 +39,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
+            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] hover:text-black sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
