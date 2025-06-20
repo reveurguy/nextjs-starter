b7f8f3b910676d2adf66cbf0e034f41480796ece
Priyanshu Singh <reveur.guy99@gmail.com>
2025-06-20 15:56:09 +0530

    chore: upgraded next to v15, tailwind to v4

--- app/page.tsx
+++ app/page.tsx
@@ -3,35 +3,36 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <Image
-          className="invert"
-          src="https://nextjs.org/icons/next.svg"
+          src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-          <li className="mb-2">
+        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
             Get started by editing{' '}
-            <code className="rounded bg-white/[.1] px-1 py-0.5 font-semibold">
+            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
               app/page.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
         </ol>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
-            className="group flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-primary px-4 text-sm text-background transition-colors hover:bg-[#ccc] hover:text-black sm:h-12 sm:px-5 sm:text-base"
+            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
             <Image
-              className="group-hover:invert"
-              src="https://nextjs.org/icons/vercel.svg"
+              className="dark:invert"
+              src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -39,7 +40,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] hover:text-black sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
+            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
@@ -47,7 +48,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -55,7 +56,7 @@ export default function Home() {
           rel="noopener noreferrer">
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/file.svg"
+            src="/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -69,7 +70,7 @@ export default function Home() {
           rel="noopener noreferrer">
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/window.svg"
+            src="/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -83,7 +84,7 @@ export default function Home() {
           rel="noopener noreferrer">
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
+            src="/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
